3ebcb276515de87a17b05273c6edbe6f88710f4a
Kyra Moore <kyra.moore21@yahoo.com>
2024-05-15 17:03:56 -0400

    added products class, validation

---
+++ PointOfSale/Products.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale
+{
+    public class Products
+    {
+        //properties
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        //constructor 
+        public Products(string _name, string _category, decimal _price, int _stock)
+        {
+            Name = _name;
+            Category = _category;
+            Description = "Central Perk";
+            Price = _price;
+            Stock = _stock;
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return $"{Description,-10} {Name,-10} {Category,-30}  {Price,0:C} \t{Stock,20}";
+        }
+
+        public static int ReduceStock(int CurrentStock, int Buy)
+        {
+            int NewStock = CurrentStock - Buy;
+            return NewStock;
+        }
+    }
+}
--- PointOfSale/Program.cs
+++ PointOfSale/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PointOfSale;
+using StaticClass;
+
+List<Products> PerkItems = new List<Products>()
+{
+    new Products("Black Drip Coffee", "Drinks", 3.50m, 50),
+    new Products("Hot Chocolate", "Drinks", 4.25m, 45),
+    new Products("Tea", "Drinks", 5.00m, 30),
+    new Products("Bottled Water", "Drinks", 2.00m, 100),
+    new Products("Pour Over Coffee", "Drinks", 6.50m, 64),
+    new Products("Iced Lavender Latte", "Drinks", 5.00m, 23),
+    new Products("Souvenir Tumbler", "Accessories", 12.99m, 60),
+    new Products("Cup Cozy", "Accessories", 8.00m, 65),
+    new Products("Mug Warmer", "Accessories", 18.99m, 25),
+    new Products("TShirt", "Accessories", 26.99m, 35),
+    new Products("Ground Coffee Bean", "Accessories", 12.99m, 20),
+    new Products("Whole Coffee Bean", "Accessories", 10.99m, 20)
+
+};
+
+//display 
+DisplayMenu(PerkItems);
+
+Console.WriteLine("What item would you like?");
+int choice = -1;
+while(!int.TryParse(Console.ReadLine(), out choice) || choice <= 0 || choice >= PerkItems.Count)
+{
+    Console.WriteLine("Invalid Input, please try again");
+}
+Console.WriteLine($"How many {PerkItems[choice - 1].Name}s would you like?");
+
+//stock to match properties
+int stock = 0;
+while(!int.TryParse(Console.ReadLine(),out stock) || stock >= PerkItems[choice -1].Stock || stock < 0 || stock == 0)
+{
+    Console.WriteLine("Invalid Input, please try again");
+}
+
+//reduces the stock 
+PerkItems[choice - 1].Stock = Products.ReduceStock(PerkItems[choice - 1].Stock, stock);
+//checking to see if worked
+DisplayMenu(PerkItems);
+
+
+//Console.WriteLine(PerkItems[choice - 1].Stock);
+
+
+
+
+
+
+
+static void DisplayMenu(List<Products> items)
+{
+    int count = 1;
+    foreach (Products p in items)
+    {
+
+        Console.WriteLine($"{count}. { p.ToString()}");
+        count++;
+    }
+}
