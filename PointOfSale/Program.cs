dd64f96d3398e1089128c2bc9f675263c6fea141
Kyra Moore <kyra.moore21@yahoo.com>
2024-05-17 11:24:58 -0400

    description edits and example of formatting

--- PointOfSale/Program.cs
+++ PointOfSale/Program.cs
@@ -7,17 +7,17 @@ using System.Security.Cryptography.X509Certificates;
 
 List<Products> PerkItems = new List<Products>()
 {
-    new Products("Black Drip Coffee","Chicago Black and Blue", "Drinks", 3.50m, 50),
-    new Products("Hot Chocolate","Ooozing with goodness!", "Drinks", 4.25m, 45),
+    new Products("Black Drip Coffee","Pure, bold, invigorating simplicity.", "Drinks", 3.50m, 50),
+    new Products("Hot Chocolate","Rich Cocoa Bliss", "Drinks", 4.25m, 45),
     new Products("Tea","Mystery Medicine!", "Drinks", 5.00m, 30),
     new Products("Bottled Water", "Straight from the Chicago River!", "Drinks", 2.00m, 100),
-    new Products("Pour Over Coffee","The pretentious choice!", "Drinks", 6.50m, 64),
-    new Products("Iced Lavender Latte","If romance is on your mind...", "Drinks", 5.00m, 23),
-    new Products("Souvenir Tumbler","For something you will never need!", "Accessories", 12.99m, 60),
+    new Products("Pour Over Coffee","The Pretentious choice!", "Drinks", 6.50m, 64),
+    new Products("Iced Lavender Latte","The Hipster Choice", "Drinks", 5.00m, 23),
+    new Products("Souvenir Tumbler","Collector's Edition", "Accessories", 12.99m, 60),
     new Products("Cup Cozy","Only if you need to ramble...", "Accessories", 8.00m, 65),
     new Products("Mug Warmer","We know you're forgetful!", "Accessories", 18.99m, 25),
-    new Products("TShirt","Wrap yourself in this hot shot!","Accessories", 26.99m, 35),
-    new Products("Ground Coffee Bean","Rise and Groan!", "Accessories", 12.99m, 20),
+    new Products("TShirt","Represent your local coffee shop!","Accessories", 26.99m, 35),
+    new Products("Ground Coffee Bean","Rise and Shine!", "Accessories", 12.99m, 20),
     new Products("Whole Coffee Bean","Rise and Grind!", "Accessories", 10.99m, 20)
 
 };
@@ -36,7 +36,7 @@ do
 {
 
 
-    Console.WriteLine($"{item} Description                       Category                  Price         Current Stock\n");
+    Console.WriteLine($"{"Item",4} Description                       Category                  Price         Current Stock\n");
     DisplayMenu(PerkItems);
 
     Console.WriteLine("Which item would you like?");
